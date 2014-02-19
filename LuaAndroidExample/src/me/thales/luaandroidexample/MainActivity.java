0299a1711a4a5963e683242e42ab59d977684e0f
thales.pereira <thalesfp@gmail.com>
2014-02-19 17:28:05 -0300

    First release

---
+++ LuaAndroidExample/src/me/thales/luaandroidexample/MainActivity.java
@@ -0,0 +1,16 @@
+package me.thales.luaandroidexample;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import org.keplerproject.luajava.*;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
